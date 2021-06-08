5061ed206725155b75784acd1cbbcc7dcddc1f56
Paul Statezny <paulstatezny@gmail.com>
2021-06-08 13:24:33 -0700

    Convert mix task to be ran as a dependency of a project

--- lib/mix/tasks/surface/convert_atom_string_shorthand.ex
+++ lib/mix/tasks/surface/convert_atom_string_shorthand.ex
@@ -1,20 +1,19 @@
 defmodule Mix.Tasks.Surface.ConvertAtomStringShorthand do
   use Mix.Task
 
-  def run(args) do
-    {_, [directory]} = OptionParser.parse!(args, strict: [])
-    File.cd!(directory)
-    compile_and_maybe_repair(directory)
+  def run(_args) do
+    compile_and_maybe_repair()
   end
 
-  def compile_and_maybe_repair(directory) do
-    with {:error, %{status: 1, err: errors}} <- Rambo.run("mix", ["compile", "--force", "--warnings-as-errors"], cd: directory) do
+  def compile_and_maybe_repair do
+    with {:error, %{status: 1, err: errors}} <-
+           Rambo.run("mix", ["compile", "--force", "--warnings-as-errors"]) do
       ~r/warning: automatic conversion of string literals into atoms is deprecated and will be removed in v0.5.0.\n\nHint: replace `(.*)` with `(.*)`\n\n  (.*):(.*):/
       |> Regex.scan(errors, capture: :all_but_first)
       |> Enum.each(&repair/1)
     end
 
-    IO.puts "✅ Finished"
+    IO.puts("✅ Finished")
   end
 
   defp repair([find, replace, filepath, line_number]) do
